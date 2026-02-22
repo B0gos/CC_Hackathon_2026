ce0536912da9a2d8c9204638fb3c8a108f65a795
B0gos <admin@perhapsbot.dev>
2026-02-21 23:57:31 -0800

    fix: readded header fix

--- my-app/app/_layout.tsx
+++ my-app/app/_layout.tsx
@@ -1,22 +1,10 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-<<<<<<< HEAD
-  return 
-    <SettingsProvider>
-      <Stack
-        screenOptions={{
-          headerShown: false,
-        }}>
-      <Stack.Screen name="index" />
-      </Stack>
-    </SettingsProvider>;
-=======
-  return <Stack
+return <Stack
     screenOptions={{
       headerShown: false,
     }}>
       <Stack.Screen name="index" />
     </Stack>;
->>>>>>> d300aab2ea235eb29ee2abd9cbe099f7b1e42e15
 }
